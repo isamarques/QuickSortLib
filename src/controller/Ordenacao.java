5217d7c293ae7e789f2095391cd5cc61e7855de0
Isabela Marques <88113608+isamarques@users.noreply.github.com>
2022-09-04 13:20:50 -0300

    Add files via upload

---
+++ src/controller/Ordenacao.java
@@ -0,0 +1,51 @@
+package controller;
+
+public class Ordenacao {
+
+	public Ordenacao() {
+		super();
+	}
+	
+	public int [] quickSort(int[] vetor, int inicio, int fim) {
+		if (fim > inicio) {
+			int posicaoFixa = dividir(vetor, inicio, fim);
+			quickSort(vetor, inicio, posicaoFixa - 1);
+			quickSort(vetor, posicaoFixa + 1, fim);
+		}
+		
+		return vetor;
+	}
+	
+	public int dividir(int vetor[], int inicio, int fim) {
+		int pivo = vetor[inicio];
+		int ponteiroEsquerda = inicio + 1;
+		int ponteiroDireita = fim;
+		
+		while (ponteiroEsquerda <= ponteiroDireita) {
+			while (ponteiroEsquerda <= ponteiroDireita && vetor[ponteiroEsquerda] <= pivo) {
+				ponteiroEsquerda++;
+			}
+			
+		while (ponteiroDireita >= ponteiroEsquerda && vetor[ponteiroDireita] > pivo) {
+			ponteiroDireita--;
+		}
+		
+		if (ponteiroEsquerda < ponteiroDireita){
+			trocar(vetor, ponteiroEsquerda, ponteiroDireita);
+			ponteiroEsquerda++;
+			ponteiroDireita--;
+		}
+	}
+		
+		trocar(vetor, inicio, ponteiroDireita);
+		return ponteiroDireita;
+		
+	}
+	
+	private void trocar(int[] vetor, int i, int j) {
+		int auxiliar = vetor[i];
+		vetor[i] = vetor[j];
+		vetor[j] = auxiliar;
+	}
+
+}
